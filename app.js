7b07f80c9a83292e648f4fb57e5baf905c95f7bd
Gömböcz Zsolt <zsolt.gombocz00@gmail.com>
2020-04-06 08:59:35 +0200

    UserSchema & Login route

--- app.js
+++ app.js
@@ -2,21 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
 
-require('dotenv/config')
 require('./database/db.js')
 
 //IMPORT ROUTES
 const postsRoute = require('./routes/posts')
 const authRoute = require('./routes/auth')
+const loginRoute = require("./routes/login")
 
 //MIDDLEWARE
 app.use(bodyParser.json())
 
 app.use('/posts', authRoute, postsRoute)
 
-app.post('/login', (req,res) => {
-   res.send("asd")
-})
+app.use('/login', loginRoute)
 
 const port = process.env.PORT || 3000
 
---
+++ models/User.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose")
+
+const UserSchema = mongoose.Schema({
+    username: {
+        type: String,
+        require: true,
+        minlength: 6,
+        maxlength: 64
+    },
+    password: {
+        type: String,
+        require: true,
+        minlength: 6,
+        maxlength: 255
+    },
+    email: {
+        verified: {type: Boolean, default: false},
+        type: String,
+        require: true,
+        minlength: 11,
+        maxlength: 64
+    },
+    permissions: {
+        verified: {type: Boolean, default: false, require:true},
+        admin: {type: Boolean, default: false, require:true},
+        server: {type: Number, default: 0, require:true}
+    },
+    image: {
+        type: String,
+        require: true,
+        default: ""
+    },
+    reg_date: {
+        type: Date,
+        default: Date.now(),
+        require: true
+    }
+
+});
+
+module.exports = mongoose.model("User", UserSchema);
