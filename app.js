c029173e8ca0f8b5cb32fee04bd650ba6621601e
Gömböcz Zsolt <zsolt.gombocz00@gmail.com>
2020-04-25 09:12:27 +0200

    Two middlewares added
    Simple notFound and errorHandler middlewares added

--- app.js
+++ app.js
@@ -2,24 +2,29 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-
-require('./database/db.js')
-
+const {notFound, errorHandler} = require('./middlewares')
 const morgan = require('morgan')
 
-//IMPORT ROUTES
+//Import Database connection file
+require('./database/db.js')
+
+//Import Routes
 const postsRoute = require('./routes/posts')
 const authRoute = require('./routes/auth')
 const userRoute = require('./routes/user')
 
-//MIDDLEWARE
+//Middlewares
 app.use(bodyParser.json())
 app.use(cors())
 app.use(morgan('[AUTO-LOG] :remote-addr - :remote-user [:date[web]]: ":method :url HTTP/:http-version" Status: :status :res[content-length] ":referrer" ":user-agent" Header: [:res[header]][:res[header]]"'))
 
+//Routes
 app.use('/posts', authRoute, postsRoute)
-
 app.use('/user', userRoute)
+
+//ErrorHandler Middlewares
+app.use(notFound);
+app.use(errorHandler);
 
 const port = process.env.PORT || 3000
 
