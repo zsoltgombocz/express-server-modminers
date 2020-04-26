c514d0b504a8b7f20f44f2d531f8a0819a919ccf
Gömböcz Zsolt <zsolt.gombocz00@gmail.com>
2020-04-26 13:07:10 +0200

    -||-

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const userModel = require('../models/User')
 const bcrypt = require('bcrypt')
 const {loginValidation, registerValidation, emailValidation} = require('../validation')
-const morgan = require('morgan')
 
 
 router.post('/register', async (req,res) => {
@@ -13,8 +12,8 @@ router.post('/register', async (req,res) => {
         error.details.forEach(e => {
             msg[e.path] = e.message;
         });
-        console.log('[LOG] Hiba a regisztráció során!\n[LOG] Kapott adat:'+JSON.stringify(req.body)+'\n[LOG] Kapott hiba:' + JSON.stringify(msg))
-        return res.status(400).send(msg)
+         console.log('[LOG] Hiba a regisztráció során!\n[LOG] Kapott adat:'+JSON.stringify(req.body)+'\n[LOG] Kapott hiba:' + JSON.stringify(msg))
+         return res.status(400).send(msg)
     }else{
         const usernameExist = await userModel.findOne({username: req.body.username})
         if(usernameExist) msg["username"] = "A felhasználónév foglalt!"; //return res.status(400).json({message: "A felhasználónév foglalt!"})
@@ -55,7 +54,8 @@ router.post('/login', async (req,res) => {
     if(error){
         error.details.forEach(e => {
             msg[e.path] = e.message;
-        });console.log('[LOG] Hiba a belépés során!\n[LOG] Kapott adat:'+JSON.stringify(req.body)+'\n[LOG] Kapott hiba:' + JSON.stringify(msg))
+        });
+        console.log('[LOG] Hiba a belépés során!\n[LOG] Kapott adat:'+JSON.stringify(req.body)+'\n[LOG] Kapott hiba:' + JSON.stringify(msg))
         return res.status(400).send(msg)
     }else{
         const loginUser = await userModel.findOne({username:req.body.username});
--- validation.js
+++ validation.js
@@ -1,18 +1,32 @@
 const joi = require('@hapi/joi');
 
+const message = {
+    "string.base": `Karaktereket kell tartalmaznia!`,
+    "string.empty": `Nem lehet üres!`,
+    "string.min": `Minimum {#limit} karakter!`,
+    "string.max": `Maximum {#limit} karakter!`,
+    "any.required": `Kitöltése kötelező!`
+}
+
 const registerValidation = data => {
     const schema = joi.object().keys({
-        username: joi.string().min(6).required(),
-        email: joi.string().min(6).required().email(),
-        description: joi.string().min(20).required(),
-        password: joi.string().min(6).required()
+        username: joi.string().min(6).required().messages(message),
+        email: joi.string().min(6).required().email().messages(message),
+        description: joi.string().min(20).required().messages(message),
+        password: joi.string().min(6).required().messages(message)
     });
     return schema.validate(data, {abortEarly: false});
 }
 
 const loginValidation = data => {
     const schema = joi.object({
-        username: joi.string().min(6).required(),
+        username: joi.string().min(6).required().messages({
+            "string.base": `"username" should be a type of 'text'`,
+            "string.empty": `"username" cannot be an empty field444444`,
+            "string.min": `"username" should have a minimum length of {#limit}`,
+            "string.max": `"username" should have a maximum length of {#limit}`,
+            "any.required": `"username" is a required field`
+          }),
         password: joi.string().min(6).required()
     });
     return schema.validate(data, {abortEarly: false});
